2eec528e3a7522a90eecfcd6adf11151620c83b5
Jimmy <jimmyrss1102@gmail.com>
2022-12-12 04:18:11 +0800

    ci [config] 使用`@`作为`src`文件夹别名

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 import electron from 'vite-plugin-electron'
 import renderer from 'vite-plugin-electron-renderer'
 import pkg from './package.json'
+import { join } from "path";
 
 rmSync('dist-electron', { recursive: true, force: true })
 const sourcemap = !!process.env.VSCODE_DEBUG
@@ -59,6 +60,13 @@ export default defineConfig({
       nodeIntegration: true,
     }),
   ],
+   //添加快捷路径@
+    // https://vitejs.dev/config/#resolve-alias
+    resolve: {
+      alias: {
+        '@': join(__dirname, "src"),
+      }
+    },
   server: process.env.VSCODE_DEBUG ? (() => {
     const url = new URL(pkg.debug.env.VITE_DEV_SERVER_URL)
     return {
