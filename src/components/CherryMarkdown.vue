451630e8dfd7ded47040200123ae031649ae98d4
Jimmy <jimmyrss1102@gmail.com>
2022-12-18 02:29:46 +0800

    feat! [cherry-markdown] : 初始化`cherry-markdown`

---
+++ src/components/CherryMarkdown.vue
@@ -0,0 +1,22 @@
+<template>
+    <div ref="cherryMarkdown"></div>
+</template>
+
+<script setup lang="ts">
+import Cherry from 'cherry-markdown';
+import Editor from 'cherry-markdown';
+import { nextTick } from 'process';
+import { ref } from 'vue';
+const cherryMarkdown = ref<Editor | null>(null)
+const cherryInstance: Editor = new Cherry({
+    id: 'cherry-markdown',
+    value: 'welcome',
+});
+nextTick(() => {
+    cherryMarkdown.value = cherryInstance
+})
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <CherryMarkdown></CherryMarkdown>
+</template>
+
+<script setup lang="ts">
+import CherryMarkdown from '@/components/CherryMarkdown.vue';
+</script>
